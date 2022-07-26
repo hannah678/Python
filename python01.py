e2b9939d56b3b4f3a3a0d24bc781c365d0582653
Hyeyoung Kim <hyeyoung.dev@gmail.com>
2022-07-26 22:25:39 +0900

    Create python01.py

---
+++ python01.py
@@ -0,0 +1,15 @@
+print("Hello World!")
+
+#조건문
+temp = 10
+if temp>20 :
+    print("덥다")
+else :
+    print("춥다")
+
+#반복문
+sign = "stop"
+
+while sign == "stop":
+    sign = input("신호를 입력하시오: ")
+print("OK 진행합니다!")
